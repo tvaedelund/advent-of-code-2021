a1b9ef3e0684e9c451cc24c2788aa5ff9ff19ed5
Tomas Vaedelund <tvaedelund@gmail.com>
2021-12-03 07:21:55 +0100

    day03 part1

---
+++ Day03/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+Console.WriteLine($"Part 1: {Solve(input)}");
+
+int Solve(string[] input)
+{
+    var minmax = Enumerable.Range(0, input[0].Length)
+        .Select(i => input.Select(x => x[i]))
+        .Select(x => x.GroupBy(y => y))
+        .Select(g => g.Select(x => new
+        {
+            key = int.Parse(x.Key.ToString()),
+            cnt = x.Count()
+        })
+            .OrderBy(x => x.cnt)
+            .ToArray()
+        )
+        .Select(x => new MinMax(x[0].key, x[1].key));
+
+    var min = Convert.ToInt32(string.Join("", minmax.Select(x => x.Min)), 2);
+    var max = Convert.ToInt32(string.Join("", minmax.Select(x => x.Max)), 2);
+
+    return min * max;
+}
+
+record MinMax(int Min, int Max);
