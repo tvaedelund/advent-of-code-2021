4e85f79ad85388fcaab2541e3a076c28b694e515
Tomas Vaedelund <tvaedelund@gmail.com>
2021-12-06 07:49:00 +0100

    day06 part 1

---
+++ Day06/Program.cs
@@ -0,0 +1,29 @@
+﻿var input = File.ReadAllText("input.txt")
+    .Split(',')
+    .Select(int.Parse)
+    .ToList();
+
+Console.WriteLine($"Part1: {Solve(input)}");
+
+int Solve(List<int> data)
+{
+    for (int day = 1; day <= 80; day++)
+    {
+        var newBorns = new List<int>();
+        data = data.Select(f =>
+        {
+            if (--f < 0)
+            {
+                newBorns.Add(8);
+                return 6;
+            }
+
+            return f;
+        })
+        .ToList();
+
+        data.AddRange(newBorns);
+    }
+
+    return data.Count();
+}
