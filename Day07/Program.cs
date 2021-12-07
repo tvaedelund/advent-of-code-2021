147c0ad8638b1da7cdda8d895477062acd30943e
Tomas Vaedelund <tomas.vaedelund@abetong.se>
2021-12-07 15:34:33 +0100

    day07 optimized

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -4,29 +4,25 @@ var sw = Stopwatch.StartNew();
 
 var input = File.ReadAllText("input.txt")
     .Split(',')
-    .Select(int.Parse);
+    .Select(int.Parse)
+    .ToArray();
 
-var result = Solve(input);
-
-Console.WriteLine($"Part1: {result.p1}");
-Console.WriteLine($"Part2: {result.p2}");
+Console.WriteLine($"Part1: {Solve1(input)}");
+Console.WriteLine($"Part2: {Solve2(input)}");
 Console.WriteLine($"TID DET TOG: {sw.ElapsedMilliseconds}ms");
 
-(long p1, long p2) Solve(IEnumerable<int> data)
+int Solve1(int[] data)
 {
-    var max = data.Max();
-    var min = data.Min();
+    return Enumerable.Range(data.Min(), data.Max())
+        .AsParallel()
+        .Min(step => data.Sum(pos => Math.Abs(pos - step)));
+}
 
-    var p1 = new List<(int step, long fuel)>();
-    var p2 = new List<(int step, long fuel)>();
-
-    for (int i = min; i < max; i++)
-    {
-        p1.Add((i, data.Sum(x => x >= i ? x - i : i - x)));
-        p2.Add((i, data.Sum(x => GetFuel(x >= i ? x - i : i - x))));
-    }
-
-    return (p1.OrderBy(x => x.fuel).First().fuel, p2.OrderBy(x => x.fuel).First().fuel);
+long Solve2(int[] data)
+{
+    return Enumerable.Range(data.Min(), data.Max())
+        .AsParallel()
+        .Min(step => data.Sum(pos => GetFuel(Math.Abs(pos - step))));
 }
 
 long GetFuel(int fuel)
