598ce83d49673d9e0ddaf6ce5cbbb52970851189
Tomas Vaedelund <tvaedelund@gmail.com>
2021-12-11 10:47:49 +0100

    day10 improvements

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -4,64 +4,71 @@ var sw = Stopwatch.StartNew();
 
 var input = File.ReadAllLines("input.txt");
 
-var chunks = new[]
-{
-    ('(', ')', 3, 1),
-    ('[', ']', 57, 2),
-    ('{', '}', 1197, 3),
-    ('<', '>', 25137, 4)
-};
-
-var result = Solve(input);
-Console.WriteLine($"Part 1: {result.p1}");
-Console.WriteLine($"Part 2: {result.p2}");
+Console.WriteLine($"Part 1: {Solve(input, true)}");
 Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
-(int p1, long p2) Solve(string[] input)
+sw.Restart();
+Console.WriteLine($"Part 2: {Solve(input, false)}");
+Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+
+long Solve(string[] input, bool getSyntaxErrorScore)
 {
-    var p1 = new List<int>();
-    var p2 = new List<long>();
+    var result = new List<long>();
 
     foreach (var line in input)
     {
-        var stack = new Stack<char>();
-        var isCorrupt = false;
+        result.Add(GetScore(line, getSyntaxErrorScore));
+    }
 
-        foreach (var ch in line)
+    if (getSyntaxErrorScore)
+    {
+        return result.Sum();
+    }
+
+    var part2 = result.Where(x => x > 0);
+    return part2.OrderBy(x => x).ElementAt(part2.Count() / 2);
+}
+
+long GetScore(string line, bool getSyntaxErrorScore)
+{
+    var chunks = new[]
+    {
+        ('(', ')', 3, 1),
+        ('[', ']', 57, 2),
+        ('{', '}', 1197, 3),
+        ('<', '>', 25137, 4)
+    };
+
+    var stack = new Stack<char>();
+
+    foreach (var ch in line)
+    {
+        // opening
+        if (chunks.Select(c => c.Item1).Any(c => c == ch))
         {
-            // opening
-            if (chunks.Select(c => c.Item1).Any(c => c == ch))
+            stack.Push(ch);
+        }
+        // closing
+        else
+        {
+            // if matching opening exists remove it from stack
+            var chunk = chunks.Single(c => c.Item2 == ch);
+
+            if (stack.Peek() == chunk.Item1)
             {
-                stack.Push(ch);
+                stack.Pop();
             }
-            // closing
+            // else it's corrupt
             else
             {
-                // if matching opening exists remove it from stack
-                var chunk = chunks.Single(c => c.Item2 == ch);
-                if (stack.Peek() == chunk.Item1)
-                {
-                    stack.Pop();
-                }
-                // else it's corrupt
-                else
-                {
-                    // part 1
-                    p1.Add(chunk.Item3);
-                    isCorrupt = true;
-                    break;
-                }
+                // part 1
+                return getSyntaxErrorScore ? chunk.Item3 : 0;
             }
-        }
-
-        if (!isCorrupt)
-        {
-            // part 2
-            p2.Add(stack.Select(x => chunks
-                    .Single(c => c.Item1 == x).Item4)
-                .Aggregate(0L, (a, v) => a * 5 + v));
         }
     }
 
-    return (p1.Sum(), p2.OrderBy(x => x).ElementAt(p2.Count / 2));
+    // part 2
+    return getSyntaxErrorScore ? 0 : stack.Select(x => chunks
+                    .Single(c => c.Item1 == x).Item4)
+                .Aggregate(0L, (a, v) => a * 5 + v);
 }
