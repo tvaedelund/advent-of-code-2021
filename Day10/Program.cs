15ab4edbb2c0b8e41e82c549e3303120d6a4f5b8
Tomas Vaedelund <tvaedelund@gmail.com>
2021-12-11 10:24:04 +0100

    day10 fixes

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -37,7 +37,7 @@ Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
             // closing
             else
             {
-                // if matching opening exists remove it
+                // if matching opening exists remove it from stack
                 var chunk = chunks.Single(c => c.Item2 == ch);
                 if (stack.Peek() == chunk.Item1)
                 {
@@ -46,7 +46,7 @@ Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
                 // else it's corrupt
                 else
                 {
-                    // part1
+                    // part 1
                     p1.Add(chunk.Item3);
                     isCorrupt = true;
                     break;
@@ -56,6 +56,7 @@ Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
         if (!isCorrupt)
         {
+            // part 2
             p2.Add(stack.Select(x => chunks
                     .Single(c => c.Item1 == x).Item4)
                 .Aggregate(0L, (a, v) => a * 5 + v));
