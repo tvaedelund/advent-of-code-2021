957ead2a0be2595f5d37176d2f63292fde6f1768
Tomas Vaedelund <tvaedelund@gmail.com>
2021-12-12 21:02:30 +0100

    day03 improved

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,7 +1,15 @@
-﻿var input = File.ReadAllLines("input.txt");
+﻿using System.Diagnostics;
 
+var sw = Stopwatch.StartNew();
+
+var input = File.ReadAllLines("input.txt");
 Console.WriteLine($"Part 1: {Solve1(input)}");
+Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+
+sw.Restart();
+Console.WriteLine();
 Console.WriteLine($"Part 2: {Solve2(input)}");
+Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
 int Solve1(string[] input)
 {
