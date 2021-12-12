555a0467bbf23abd49d20cc6eb7bc5b09249faf1
Tomas Vaedelund <tvaedelund@gmail.com>
2021-12-12 21:04:51 +0100

    day12 templated

---
+++ Day12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
+
+var input = File.ReadAllLines("input.txt");
+Console.WriteLine($"Part 1: {Solve(input)}");
+Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+
+// sw.Restart();
+// Console.WriteLine();
+// Console.WriteLine($"Part 2: {Solve(input)}");
+// Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+
+int Solve(string[] input)
+{
+    return 0;
+}
