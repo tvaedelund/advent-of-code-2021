5eb816b759f0ff35d6619c8fdac2cd900934edbb
Tomas Vaedelund <tomas.vaedelund@abetong.se>
2021-12-14 11:17:42 +0100

    day08 improved?

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -10,30 +10,26 @@ var input = File.ReadAllLines("input.txt")
     )
     .ToArray();
 
+// Key: Digit
+// Value: Length
 var digits = new Dictionary<int, int>
 {
-    {0, 6},
     {1, 2},
-    {2, 5},
-    {3, 6},
     {4, 4},
-    {5, 5},
-    {6, 6},
     {7, 3},
     {8, 7},
-    {9, 6},
 };
 
-Console.WriteLine($"Part1: {Solve1(input, new[] {1, 4, 7, 8})}");
+Console.WriteLine($"Part1: {Solve1(input)}");
 Console.WriteLine($"Part2: {Solve2(input)}");
 Console.WriteLine($"TID DET TOG: {sw.ElapsedMilliseconds}ms");
 
-int Solve1(string[][][] input, int[] digitsToFind)
+int Solve1(string[][][] input)
 {
     var result = input.Select(entry => entry[1])
         .SelectMany(value => value)
         .Select(value => digits
-            .Where(digit => digit.Value == value.Length && digitsToFind.Any(dtf => dtf == digit.Key))
+            .Where(digit => digit.Value == value.Length)
         )
         .Where(found => found.Any());
 
