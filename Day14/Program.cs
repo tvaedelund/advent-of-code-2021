cf9e58c5ee4305754150bd882fe45e31d8e462a7
Tomas Vaedelund <tomas.vaedelund@abetong.se>
2021-12-15 12:26:20 +0100

    Day14 part 2

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.Diagnostics;
 
 var sw = Stopwatch.StartNew();
-var input = File.ReadAllLines("_input.txt");
+var input = File.ReadAllLines("input.txt");
 
 Console.WriteLine($"Part 1: {Solve(input, 10)}");
 Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
-// sw.Restart();
-// Console.WriteLine();
-// Console.WriteLine($"Part 2: {Solve(input, 100)}");
-// Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+Console.WriteLine();
+Console.WriteLine($"Part 2: {Solve(input, 40)}");
+Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
-int Solve(string[] input, int v)
+long Solve(string[] input, int v)
 {
     var polymer = Enumerable.Zip(input[0], input[0].Skip(1))
         .Select(p => $"{p.First}{p.Second}")
@@ -19,7 +19,7 @@ int Solve(string[] input, int v)
         .Select(grp => new
         {
             chr = grp.Key,
-            cnt = grp.Count()
+            cnt = (long)grp.Count()
         })
         .ToDictionary(grp => grp.chr, grp => grp.cnt);
 
@@ -28,23 +28,23 @@ int Solve(string[] input, int v)
         .Select(grp => new
         {
             chr = grp.Key,
-            cnt = grp.Count()
+            cnt = (long)grp.Count()
         })
         .ToDictionary(grp => grp.chr, grp => grp.cnt);
 
     var formula = input[2..]
         .Select(x => x.Split(" -> "))
-        .ToDictionary(x => x[0], x=> x[1][0]);
+        .ToDictionary(x => x[0], x => x[1][0]);
 
     for (int step = 1; step <= v; step++)
     {
-        var found = new Dictionary<string, int>();
+        var found = new Dictionary<string, long>();
         foreach (var ab in polymer.Keys)
         {
-            (char a, char n, char b, int count) = (ab[0], formula[ab], ab[1], found.GetValueOrDefault(ab));
+            (char a, char n, char b, long count) = (ab[0], formula[ab], ab[1], polymer.GetValueOrDefault(ab));
 
-            found[$"{a}{n}"] = polymer.GetValueOrDefault($"{a}{n}") + count;
-            found[$"{n}{b}"] = polymer.GetValueOrDefault($"{n}{b}") + count;
+            found[$"{a}{n}"] = found.GetValueOrDefault($"{a}{n}") + 1 * count;
+            found[$"{n}{b}"] = found.GetValueOrDefault($"{n}{b}") + 1 * count;
 
             elements[n] = elements.GetValueOrDefault(n) + count;
         }
