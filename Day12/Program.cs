2a563096c30d5a02ee9685e539adfe47da92e908
Tomas Vaedelund <tvaedelund@gmail.com>
2021-12-15 06:07:55 +0100

    day12 improvments in progress

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -2,7 +2,7 @@
 
 var sw = Stopwatch.StartNew();
 
-var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("____input.txt");
 Console.WriteLine($"Part 1: {Solve(input)}");
 Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
@@ -13,5 +13,51 @@ Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
 
 int Solve(string[] input)
 {
-    return 0;
+    var caves = GetCaves(input)
+        .Where(c => c.to != "start" && c.from != "end")
+        .ToList();
+
+    foreach (var cave in caves.Where(c => c.from == "start"))
+    {
+        var test = TraverseCaves(caves, cave, new() { cave.from }, 0);
+    }
+
+    return 0;    
 }
+
+int TraverseCaves(IEnumerable<Cave> caves, Cave current, List<string> visitedCaves, int count)
+{
+    var bigCave = current.from.ToUpper() == current.from;
+    var visited = visitedCaves.Select(v => v).Contains(current.to);
+    var endCave = current.to == "end";
+
+    if (endCave)
+    {
+        count++;
+    }
+    else if (bigCave || !visited)
+    {
+        visitedCaves.Add(current.to);
+
+        var cavesToVisit = caves.Where(c => c.from == current.to);
+
+        foreach (var caveToVisit in cavesToVisit)
+        {
+            count = TraverseCaves(caves, caveToVisit, visitedCaves, count);
+        }
+    }
+
+    return count;
+}
+
+IEnumerable<Cave> GetCaves(string[] data)
+{
+    foreach (var line in data)
+    {
+        var splitLine = line.Split('-');
+        yield return new Cave(splitLine[0], splitLine[1]);
+        yield return new Cave(splitLine[1], splitLine[0]);
+    }
+}
+
+record Cave(string from, string to);
